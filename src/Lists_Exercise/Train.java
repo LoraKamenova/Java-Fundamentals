1da7a24cc654fea98d3b01f701d09f3aa62a5010
Lora <cruzeiro@abv.bg>
2021-01-16 22:13:35 +0200

    Initial Commit

--- src/Lists_Exercise/Train.java
+++ src/Lists_Exercise/Train.java
@@ -1,7 +1,35 @@
 package Lists_Exercise;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Train {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        List<Integer> wagons = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt).collect(Collectors.toList());
+        int maxCapacity = Integer.parseInt(scanner.nextLine());
+
+        String input = "";
+
+        while (!"end".equals(input = scanner.nextLine())){
+            String[] data = input.split("\\s+");
+
+            if (data[0].equals("Add")){
+                wagons.add(Integer.parseInt(data[1]));
+            } else {
+                int people = Integer.parseInt(data[0]);
+                for (int i = 0; i < wagons.size(); i++) {
+                    if (wagons.get(i) + people <= maxCapacity){
+                        wagons.set(i, people + wagons.get(i));
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(wagons.toString().replaceAll("[\\[\\],]",""));
     }
 }
