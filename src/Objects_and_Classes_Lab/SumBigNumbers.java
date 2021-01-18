7b2e10b3a18c24963ffa3c8bf2d01a820db9dd23
Lora <cruzeiro@abv.bg>
2021-01-18 17:15:56 +0200

    Initial Commit

---
+++ src/Objects_and_Classes_Lab/SumBigNumbers.java
@@ -0,0 +1,17 @@
+package Objects_and_Classes_Lab;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class SumBigNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        BigInteger firstNumber = new BigInteger(scanner.nextLine());
+        BigInteger secondNumber = new BigInteger(scanner.nextLine());
+
+        BigInteger result = firstNumber.add(secondNumber);
+
+        System.out.println(result);
+    }
+}
