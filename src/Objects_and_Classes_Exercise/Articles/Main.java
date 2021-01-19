fcf2e4cfb2eaff1054acde022a0be208a36453a3
Lora <cruzeiro@abv.bg>
2021-01-19 23:16:49 +0200

    initial Commit

---
+++ src/Objects_and_Classes_Exercise/Articles/Main.java
@@ -0,0 +1,31 @@
+package Objects_and_Classes_Exercise.Articles;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(", ");
+
+        Article article = new Article(input[0], input[1], input[2]);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String[] command = scanner.nextLine().split(": ");
+
+            switch (command[0]) {
+                case "Edit":
+                    article.setContent(command[1]);
+                    break;
+                case "ChangeAuthor":
+                    article.setAuthor(command[1]);
+                    break;
+                case "Rename":
+                    article.setTitle(command[1]);
+                    break;
+            }
+        }
+        System.out.println(article.toString());
+    }
+}
