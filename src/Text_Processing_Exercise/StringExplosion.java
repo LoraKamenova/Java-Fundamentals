531da26fb6595e958d4fc588b0ef9bdc414b4e5d
Lora <cruzeiro@abv.bg>
2021-01-23 22:21:43 +0200

    Initial Commit

---
+++ src/Text_Processing_Exercise/StringExplosion.java
@@ -0,0 +1,41 @@
+package Text_Processing_Exercise;
+
+import java.util.Scanner;
+
+public class StringExplosion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+
+            if (symbol == '>') {
+                result.append(">");
+                i++;
+                int explosion = text.charAt(i) - '0';
+                explosion--;
+
+                while (explosion > 0 && i < text.length() - 1) {
+                    i++;
+                    symbol = text.charAt(i);
+                    if (symbol == '>') {
+                        result.append(">");
+                        i++;
+                        explosion += text.charAt(i) - '0';
+                        explosion--;
+                        continue;
+                    }
+                    explosion--;
+                }
+
+            } else {
+                result.append(symbol);
+            }
+        }
+
+        System.out.println(result);
+    }
+}
